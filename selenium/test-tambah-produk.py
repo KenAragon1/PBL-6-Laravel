5cc412f557fb62c3cdfc136e0ce05863f7592d4a
KenAragon1 <142734032+KenAragon1@users.noreply.github.com>
2023-12-24 21:55:47 +0700

    pindahin testing

--- selenium/test-tambah-produk.py
+++ selenium/test-tambah-produk.py
@@ -83,6 +83,7 @@ except Exception as e:
     status = "Gagal"
     print(f"Terjadi Kesalahan : {e}")
 finally:
-    with open("hasil-tambah-produk.txt", "a") as file:
+    with open("hasil/hasil-tambah-produk.txt", "a") as file:
         waktu_uji = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"{waktu_uji} - Fitur CRUD - Status : {status} \n ")
+        driver.quit()
